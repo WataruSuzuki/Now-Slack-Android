3d22c1e93696d3e69cbd077ad4a3232023afc8d7
WataruSuzuki <wataru0406@gmail.com>
2017-09-15 16:56:23 +0900

    Added fragment : account manager

--- mobile/src/main/java/jp/co/devjchankchan/now_slack_android/DrawerPages.kt
+++ mobile/src/main/java/jp/co/devjchankchan/now_slack_android/DrawerPages.kt
@@ -1,10 +1,14 @@
 package jp.co.devjchankchan.now_slack_android
 
 import android.app.Fragment
+import jp.co.devjchankchan.now_slack_android.fragments.AccountManagerFragment
 import jp.co.devjchankchan.now_slack_android.fragments.BeaconManagerFragment
 import jp.co.devjchankchan.now_slack_android.fragments.SummaryFragment
 
 enum class DrawerPages(val menuId: Int) {
+    ACCOUNT_MANAGER(R.id.nav_account) {
+        override fun createFragment(): Fragment = AccountManagerFragment.newInstance(1)
+    },
     BEACON_MANAGER(R.id.nav_beacon) {
         override fun createFragment(): Fragment = BeaconManagerFragment()
     },
